f6061531e8b6b0aab0c34a793a1ea1c1cfd917ab
codebaya <beauglory@gmail.com>
2022-04-12 21:08:56 -0400

    no-btnconn

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -21,7 +21,7 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 
 const Welcome = () => {
 
-    const { connectWallet } = useContext(TransactionContext);
+    const { connectWallet, currentAccount} = useContext(TransactionContext);
 
     const handleSubmit = () => {
 
@@ -37,16 +37,20 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrency easily on Web3
                     </p>
-                    <button
+
+                    {!currentAccount && (
+                        <button
                         type="button"
                         onClick={connectWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-                    >
+                        >
                         <AiFillPlayCircle className="text-white ml-2" />
                         <p className="text-white ml-40 mr-40 justify-center text-base font-semibold">
                             Connect Wallet
                         </p>  
-                    </button>
+                        </button>
+                    )}
+                        
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
                             Reliability
